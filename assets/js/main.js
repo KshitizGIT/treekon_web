26b28fc8d9ea0669990ff4212b356049809195ac
KshitizGIT <KshitizGIT@users.noreply.github.com>
2025-10-30 16:30:16 +0000

    deploy: e58fc812d1dd2f02bd238ed2dd59e778a7731a90

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,36 @@
 // On hover, set class active to hoverered submenu and remove from others
 
-document.querySelectorAll('.submenu').forEach(submenu => {
-    submenu.addEventListener('mouseenter', () => {
-        document.querySelectorAll('.submenu').forEach(sm => sm.classList.remove('active'));
-        submenu.classList.add('active');
-        // Only show submenu with data-target
-        
-        document.querySelectorAll('.submenu-content').forEach(content => {
-            content.classList.add('hidden');
-        });
-        const target = submenu.dataset.target;
-        const targetContent = document.getElementById(target);
-        if (targetContent) {
-            targetContent.classList.remove('hidden');
-        }
+document.querySelectorAll(".submenu").forEach((submenu) => {
+  submenu.addEventListener("mouseenter", () => {
+    document
+      .querySelectorAll(".submenu")
+      .forEach((sm) => sm.classList.remove("active"));
+    submenu.classList.add("active");
+    // Only show submenu with data-target
+
+    document.querySelectorAll(".submenu-content").forEach((content) => {
+      content.classList.add("hidden");
     });
+    const target = submenu.dataset.target;
+    const targetContent = document.getElementById(target);
+    if (targetContent) {
+      targetContent.classList.remove("hidden");
+    }
+  });
 });
 
+activateService = (service) => {
+  document.querySelectorAll(".service-button").forEach((btn) => {
+    btn.classList.remove("button-primary");
+    btn.classList.add("button-secondary");
+  });
+  const activeButton = document.getElementById("service-button-" + service);
+  activeButton.classList.add("button-primary");
+  activeButton.classList.remove("button-secondary");
+
+  document
+    .querySelectorAll(".service-image")
+    .forEach((img) => img.classList.add("hidden"));
+  const image = document.getElementById("service-image-" + service);
+  image.classList.remove("hidden");
+};
